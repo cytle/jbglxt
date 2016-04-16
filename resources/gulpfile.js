74f4978a7a2f677c0d1591e25208945fb15372db
炒饭 <chensp@59store.com>
2016-04-17 00:49:59 +0800

    change:『gulp』完成

--- resources/gulpfile.js
+++ resources/gulpfile.js
@@ -5,23 +5,54 @@ var gulp = require('gulp'),
     cssnano = require('gulp-cssnano'),
     clean = require('gulp-clean'),
     livereload = require('gulp-livereload'),
+    fs = require('fs'),
     http = require('http'),
     st = require('st'),
     exec = require('gulp-exec'),
     paths,
     watchers,
-    less2css;
+    less2css,
+    rootPath,
+    assetsPath,
+    needLessWhenProducting;
+
+rootPath = fs.realpathSync('.') + '/';
+assetsPath = rootPath + 'assets/';
 
 
 paths = {
-  root: './',
-  less: ['assets/less/style.less', 'assets/less/reset.less', 'assets/less/vendor.less'],
-  style: ['assets/less/style.less', 'assets/less/reset.less'],
-  images: 'src/images',
-  css: 'dist/css',
-  dist: 'dist',
-  src: 'src',
+  root: rootPath,
+  less: assetsPath + 'less/',
+  images: assetsPath + 'images/',
+  js: assetsPath + 'js/',
+  css: rootPath + 'dist/css/',
+  dist: rootPath + 'dist/',
 };
+
+needLessWhenProducting = [paths.less + 'style.less', paths.less + 'vendor.less'];
+
+
+// 监听配置
+watchers = [
+  {
+    paths: [
+      paths.less + 'style.less',
+      paths.less + 'style/*.less'],
+    executor: function () {
+      return less2css([paths.less + 'style.less']);
+    }
+  },
+  {
+    paths: [
+      paths.less + 'vendor.less',
+      paths.less + 'variables.less',
+      paths.less + 'vendor/*.less'],
+    executor: function () {
+      return less2css([paths.less + 'vendor.less']);
+    }
+  },
+];
+
 
 less2css = function(path) {
 
@@ -36,71 +67,64 @@ less2css = function(path) {
     .pipe(livereload());
 };
 
-watchers = [
-  {
-    paths: paths.style,
-    executor: function () {
-      return less2css(paths.style);
-    }
-  },
-  {
-    paths: [
-      'assets/less/vendor.less',
-      'assets/less/variables.less',
-      'assets/less/vendor/*.less'],
-    executor: function () {
-      return less2css(['assets/less/vendor.less']);
-    }
-  },
-];
+// 初始化
+gulp.task('init', ['clean'], function () {
+
+  // 创建dist文件夹
+  gulp.src([paths.root])
+    .pipe(exec("mkdir -p " + paths.dist));
+});
 
 
-//清空dist文件夹
+// 清空dist文件夹
 gulp.task('clean', function() {
-  return gulp.src('dist/*', {
+  return gulp.src(paths.dist, {
     read: false
     })
     .pipe(clean());
 });
 
 
-//复制图片文件到dist目录
+// 复制图片文件到dist目录
 gulp.task('images', function() {
   return gulp.src(paths.images)
     .pipe(gulp.dest(paths.dist));
 });
 
+//复制js文件到dist目录
+gulp.task('js', function() {
+  return gulp.src(paths.js)
+    .pipe(gulp.dest(paths.dist));
+});
+
 //less编译成css
 gulp.task('less', function () {
-  return less2css(paths.less);
+  return less2css(needLessWhenProducting);
 });
 
 //监听less文件改动  自动编译成css
-gulp.task('watch', ['clean'], function() {
+gulp.task('watch', ['init'], function() {
   livereload.listen({
     quiet: true
   });
 
   gulp.start('less', 'server');
 
-  gulp.src(paths.src)
-    .pipe(
-      exec([
-        "ln -s ../src/js dist/"
-      ])
-    )
-    .pipe(
-      exec([
-        "ln -s ../src/images dist/"
-      ])
-    );
+  // 链接js和images
+  gulp.src([paths.js, paths.images], {
+    read: false
+    })
+    .pipe(exec("ln -s <%= file.path %> <%= options.dist %>", {
+      dist: paths.dist
+    }));
 
+  // 添加监听
   for (var i = watchers.length - 1; i >= 0; i--) {
     gulp.watch(watchers[i].paths, watchers[i].executor);
   }
 
-
 });
+
 
 // 开始服务
 gulp.task('server', function(done) {
@@ -110,6 +134,6 @@ gulp.task('server', function(done) {
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', function(){
-  gulp.start('clean', 'images','less');
+gulp.task('default', ['init'], function(){
+  gulp.start('images', 'js','less');
 });
