9d95f270cf738de9c476111f23c3c36ff9fd2942
韩丹舟 <handanzhou@handanzhoudeMacBook-Pro.local>
2016-04-19 09:10:16 +0800

    change: 配置，修改配置

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '~a$TJBsdo7Xk]P9h?w2RU,b8Yr/4lOnVCKM-5@!q'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:qweqwe@127.0.0.1:3306/jbglxt'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:@127.0.0.1:3306/jbglxt'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'jb_'); // 数据表前缀，使用Model方式调用API必须配置此项
